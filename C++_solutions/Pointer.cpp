977d094a61be5aff9b351fb00c7242911f59a956
Samriddh Singh <samriddh.singh007@gmail.com>
2020-10-03 11:40:57 +0530

    Added codes of keil ,cpp and java

--- C++_solutions/Pointer.cpp
+++ C++_solutions/Pointer.cpp
@@ -1,31 +1,25 @@
-points=10.0
+#include <stdio.h>
 
-#include<stdio.h>
-void fun(int *x,int *y)
-{
-    int sum=*x+*y;
-    printf("%d\n",sum);
-    if(*x>*y)
-    {
-        int b2=*x-*y;
-        printf("%d\n",b2);
-    }
-    else if(*y>*x)
-    {
-        int b3=*y-*x;
-        printf("%d\n",b3);
-    }
-
-
+void update(int *a,int *b) {
+    // Complete this function 
+    
+    int c=(*a)+(*b);
+    int d;
+    if((*a)>(*b))
+    d=(*a)-(*b);
+    else if((*b)>(*a))
+    {d=(*b)-(*a);}
+    (*a)=c;
+    (*b)=d;
 }
-int main()
-{
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    int *p1;
-    p1=&a;
-    int *p2;
-    p2=&b;
-    fun(p1,p2);
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
 }
